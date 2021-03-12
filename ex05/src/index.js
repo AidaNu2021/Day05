8f9bb6375dd5bba7ebf4af70b99c4527f44b4556
AidaNu2021 <aida.nusinovic@gmail.com>
2021-03-13 00:01:40 +0100

    dodat ex05

---
+++ ex05/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+
+    var myString = "Concatenation is the process to appending ";
+    myString += "one string to the end of another string.";
+
+    return myString;
+    }
+    console.log(main());
+    module.exports = main;
